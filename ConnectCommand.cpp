cb5468046f125440f0520e7137768238ce13883d
nikitakogan1 <46023717+nikitakogan1@users.noreply.github.com>
2018-12-23 08:51:14 -0800

    Add files via upload

--- ConnectCommand.cpp
+++ ConnectCommand.cpp
@@ -5,6 +5,30 @@
 //execute method
 double ConnectCommand :: execute()
 {
+	// first parameter is the ip.
+	string ip = this->params[this->ind+1];
+	int port;
+	Expression * e1 = nullptr;
+	//handle the port, it could be an expression.
+	(this->sy)->SetExpToShunt(this->params[this->ind+2]);
+	try
+	{
+		e1 = this->sy->Shunt();
+		if (e1 == nullptr)
+		{
+			cout << "invalid paramaters passed to openDataServer function" <<endl;
+			return FAIL;
+		}
+		//get the port value.
+		port = e1->calculate();
+		delete e1;
+	}
+	catch(runtime_error &e)
+	{
+		delete e1;
+		cout << e.what() <<endl;
+		return FAIL;
+	}
 
 	return 0;
 }
