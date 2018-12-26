cb12c9b99aae8f29769029e9761f5f0341e2026e
DAv10195 <46025968+DAv10195@users.noreply.github.com>
2018-12-26 02:45:14 +0200

    Thug Life

--- RunFromFIleCommand.cpp
+++ RunFromFIleCommand.cpp
@@ -2,15 +2,20 @@
 #include "Command.h"
 #include "FlightInterp.h"
 #include "Utils.h"
+#define FAIL 1
 //execute method
 double RunFromFileCommand :: execute()
 {
-	double retCmd = 0;
+	if (this->ind == (this->params).size() - 1)
+	{
+		cout << "no path inputed to run command" << endl;
+		return FAIL;
+	}
 	Lexer* lexer = new Lexer();
 	Parser* parser = new Parser(this->ifCreated ,this->ifRun, this->tAl, this->socketId);	//accessing the file path that should be inputed next to the "run" command
 	string path = this->params[this->ind + 1];
 
-	retCmd = execFromFile(this->sTable, this->refs, this->revRefs, path, lexer, parser);
+	double retCmd = execFromFile(this->sTable, this->refs, this->revRefs, path, lexer, parser);
 
 	delete lexer;
 	delete parser;
